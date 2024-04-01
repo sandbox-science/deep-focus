8e54223dc2c750545d47311e0fa1b548f8a61804
Chris <rolletchris1@gmail.com>
2024-03-31 17:21:12 -0700

    remove uniqueness username

--- internal/database/user.go
+++ internal/database/user.go
@@ -10,7 +10,7 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Username string `gorm:"size:255;not null;" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"-"`
 	Email    string `gorm:"size:100;not null;unique" json:"email"`
 }
