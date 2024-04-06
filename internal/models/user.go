44e3865fd76425cdb60d3f86dae7b906c7c7bbe6
Chris <rolletchris1@gmail.com>
2024-04-05 22:23:10 -0700

    removed unique username

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"size:255;not null;unique" json:"username"`
+	Username string `gorm:"size:255;not null;" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"-"`
 	Email    string `gorm:"size:100;not null;unique" json:"email"`
 }
